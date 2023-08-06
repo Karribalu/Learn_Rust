d5f7a0ea9ddcede44315b223bb6499beed26a111
karribalu <karri.balu123456@gmail.com>
2023-08-06 22:40:51 +0100

    strings completed

---
+++ strings_practise/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let s1 = String::from("hello");
+    let s2 = String::from(" world");
+    let s3 = s1 + &s2;
+    println!("{s3}");
+    // println!("{s1}");
+    println!("{s2}");
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{s1}-{s2}-{s3}");
+    let some = &s1[0];
+    println!("{some}");
+}
