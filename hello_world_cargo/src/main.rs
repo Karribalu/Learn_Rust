dcd1811b980b576fea509f047b74f7eb4409126e
karribalu <karri.balu123456@gmail.com>
2023-08-18 22:18:31 +0100

    refactoring continuos

--- hello_world_cargo/src/main.rs
+++ hello_world_cargo/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
+    println!(
+        "max in i32 {} max in i64 {} in i128 {}",
+        i32::MAX,
+        i64::MAX,
+        i128::MAX
+    );
+
+    let val: i128 = 9223372036854775807 * 9223372036854775807;
+    println!("{}", val);
 }
